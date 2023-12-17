38430783eb614779f509761a9cfe8c601623f318
Leaagga <135800909+Leaagga@users.noreply.github.com>
2023-12-17 18:38:55 +0800

    8.15 database part1

---
+++ library-backend/library-author-schema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+// // you must install this library
+// const uniqueValidator = require('mongoose-unique-validator')
+
+// const uniqueValidator = require('mongoose-unique-validator')
+
+const authorSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 4,
+  },
+  born: {
+    type: Number,
+  },
+})
+
+// schema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('Author', authorSchema)
