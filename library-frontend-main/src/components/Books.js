26ea88c30544c3a9552a8f33812cba1b9c8fb620
Leaagga <135800909+Leaagga@users.noreply.github.com>
2023-12-22 18:23:33 +0800

    8.19 Books by genre, part 1

--- library-frontend-main/src/components/Books.js
+++ library-frontend-main/src/components/Books.js
@@ -1,7 +1,9 @@
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../query'
+import { useState } from 'react'
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS)
+  const [genresfilter, setGenresfilter] = useState(null)
   console.log(result)
   if (!props.show) {
     return null
@@ -11,10 +13,29 @@ const Books = (props) => {
   }
   const books = []
   books.push(...result.data.allBooks)
+  let bookGenres = []
+  if (books) {
+    books.map((b) =>
+      b.genres.forEach((element) => {
+        console.log(element)
+        if (!bookGenres.find((g) => g === element)) {
+          bookGenres.push(element)
+          console.log(bookGenres)
+        }
+      })
+    )
+  }
+
   return (
     <div>
       <h2>books</h2>
-
+      {genresfilter ? (
+        <div>
+          in genre <b>{genresfilter}</b>
+        </div>
+      ) : (
+        <div>all genres</div>
+      )}
       <table>
         <tbody>
           <tr>
@@ -22,15 +43,27 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter((b) =>
+              genresfilter ? b.genres.find((g) => g === genresfilter) : true
+            )
+            .map((a) => (
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      <div>
+        {bookGenres.map((g) => (
+          <button key={g} onClick={() => setGenresfilter(g)}>
+            {g}
+          </button>
+        ))}
+        <button onClick={() => setGenresfilter(null)}>all genres</button>
+      </div>
     </div>
   )
 }
